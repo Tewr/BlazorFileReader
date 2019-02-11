532a97dfb660f6fbeded52d782f8b926523da495
Tor KNUTSSON <tor.knutsson@qiminfo.ch>
2019-02-11 21:09:40 +0100

    Simplified example a bit

--- src/Blazor.FileReader.Wasm.Demo/Startup.cs
+++ src/Blazor.FileReader.Wasm.Demo/Startup.cs
@@ -20,14 +20,9 @@ namespace Blazor.FileReader.Wasm.Demo
         {
             private static MonoWebAssemblyJSRuntime MonoWebAssemblyJSRuntime = 
                 JSRuntime.Current as MonoWebAssemblyJSRuntime;
-
-            public static bool IsAvailable { get; } = 
-                MonoWebAssemblyJSRuntime != null;
-
-            public TRes InvokeUnmarshalled<T1, T2, TRes>(string identifier, T1 arg1, T2 arg2)
-            {
-                return MonoWebAssemblyJSRuntime.InvokeUnmarshalled<T1, T2, TRes>(identifier, arg1, arg2);
-            }
+            
+            public TRes InvokeUnmarshalled<T1, T2, TRes>(string identifier, T1 arg1, T2 arg2) =>
+                MonoWebAssemblyJSRuntime.InvokeUnmarshalled<T1, T2, TRes>(identifier, arg1, arg2);
         }
 
         public void Configure(IComponentsApplicationBuilder app)
