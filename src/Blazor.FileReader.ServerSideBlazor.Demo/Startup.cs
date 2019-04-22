1eb6e3b59c151fa29c90b6a603e53f68079f2e0a
Tor KNUTSSON <tor.knutsson@qiminfo.ch>
2019-04-22 17:22:38 +0200

    WIP, Server-side has serialization errors

--- src/Blazor.FileReader.ServerSideBlazor.Demo/Startup.cs
+++ src/Blazor.FileReader.ServerSideBlazor.Demo/Startup.cs
@@ -8,8 +8,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Blazor.FileReader.ServerSideBlazor.Demo.Components;
-using Blazor.FileReader.ServerSideBlazor.Demo.Services;
 
 namespace Blazor.FileReader.ServerSideBlazor.Demo
 {
