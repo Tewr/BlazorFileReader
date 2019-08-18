9141f0ddf7b4f35d18f3e746f3d45e06c6ade57a
Tor KNUTSSON <tor.knutsson@qiminfo.ch>
2019-08-18 13:28:41 +0200

    Demo laoder

--- src/Demo/Blazor.FileReader.Wasm.Demo/Program.cs
+++ src/Demo/Blazor.FileReader.Wasm.Demo/Program.cs
@@ -4,14 +4,6 @@ namespace Blazor.FileReader.Wasm.Demo
 {
     public class Program
     {
-        // Github sub-directory
-        public static readonly string BasePath
-#if GHPAGES
-             = "/BlazorFileReader";
-#else
-            = "";
-#endif
-
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
