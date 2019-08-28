4307ff7840c5121cc4f94bfacb5242714ac96bf9
Tor <Tewr@users.noreply.github.com>
2019-08-28 16:54:24 +0200

    Update Startup.cs
    ... and a using

--- src/Demo/Blazor.FileReader.ServerSide.Demo/Startup.cs
+++ src/Demo/Blazor.FileReader.ServerSide.Demo/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http.Features;
 
 namespace Blazor.FileReader.ServerSide.Demo
 {
