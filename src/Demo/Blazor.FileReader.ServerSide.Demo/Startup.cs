2bad0f9631c67d99b94c12ea11241509f93c02df
Tor <Tewr@users.noreply.github.com>
2019-08-28 17:03:50 +0200

    Workarounds for asp.net #13470 (#70)
    * Update Startup.cs
    
    * Update README.md

--- src/Demo/Blazor.FileReader.ServerSide.Demo/Startup.cs
+++ src/Demo/Blazor.FileReader.ServerSide.Demo/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http.Features;
 
 namespace Blazor.FileReader.ServerSide.Demo
 {
@@ -19,6 +20,13 @@ namespace Blazor.FileReader.ServerSide.Demo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Temporary workaround for https://github.com/aspnet/AspNetCore/issues/13470
+            app.Use(async (context, next) =>
+            {
+                context.Features.Get<IHttpMaxRequestBodySizeFeature>().MaxRequestBodySize = null;
+                await next.Invoke();
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
