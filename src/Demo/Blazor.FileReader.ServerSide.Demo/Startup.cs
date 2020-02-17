55a7ab1f80523a38e2581365719109248b7f59c5
Tor KNUTSSON <tor.knutsson@qiminfo.ch>
2020-02-17 21:59:29 +0100

    removed repro

--- src/Demo/Blazor.FileReader.ServerSide.Demo/Startup.cs
+++ src/Demo/Blazor.FileReader.ServerSide.Demo/Startup.cs
@@ -18,7 +18,6 @@ namespace Blazor.FileReader.ServerSide.Demo
                 o.MaximumReceiveMessageSize = 10 * 1024 * 1024; // 10MB
             });
             services.AddFileReaderService();
-            var types = typeof(FileReader.BrowserFileReaderException).Assembly.GetTypes();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
