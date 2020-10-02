e6de02fb37c14ece77c9aeda862cc68e7f216ac9
Tor KNUTSSON <Tewr@users.noreply.github.com>
2020-10-02 22:44:53 +0200

    Publish

--- _content/Tewr.Blazor.FileReader/FileReaderComponent.js
+++ _content/Tewr.Blazor.FileReader/FileReaderComponent.js
@@ -1,5 +1,18 @@
 ;
 ;
+;
+var FileReaderJsInterop = (function () {
+    function FileReaderJsInterop() {
+    }
+    FileReaderJsInterop.initialize = function () {
+        FileReaderJsInterop.endTask =
+            Module.mono_bind_static_method("[" + this.assembly + "] Tewr.Blazor.FileReader.FileReaderJsInterop:EndTask");
+        FileReaderJsInterop.initialized = true;
+    };
+    FileReaderJsInterop.assembly = 'Tewr.Blazor.FileReader';
+    FileReaderJsInterop.initialized = false;
+    return FileReaderJsInterop;
+}());
 var FileReaderComponent = (function () {
     function FileReaderComponent() {
         var _this = this;
@@ -7,6 +20,7 @@ var FileReaderComponent = (function () {
         this.fileStreams = {};
         this.dragElements = new Map();
         this.elementDataTransfers = new Map();
+        this.readResultByTaskId = new Map();
         this.RegisterDropEvents = function (element, additive) {
             _this.LogIfNull(element);
             var handler = function (ev) {
@@ -72,8 +86,11 @@ var FileReaderComponent = (function () {
         this.Dispose = function (fileRef) {
             return delete (_this.fileStreams[fileRef]);
         };
-        this.OpenRead = function (element, fileIndex) {
+        this.OpenRead = function (element, fileIndex, useWasmSharedBuffer) {
             _this.LogIfNull(element);
+            if (useWasmSharedBuffer && !FileReaderJsInterop.initialized) {
+                FileReaderJsInterop.initialize();
+            }
             var files = _this.GetFiles(element);
             if (!files) {
                 throw 'No FileList available.';
@@ -92,8 +109,13 @@ var FileReaderComponent = (function () {
                 bufferOffset: Blazor.platform.readUint64Field(readFileParamsPointer, 8),
                 count: Blazor.platform.readInt32Field(readFileParamsPointer, 16),
                 fileRef: Blazor.platform.readInt32Field(readFileParamsPointer, 20),
-                position: Blazor.platform.readUint64Field(readFileParamsPointer, 24),
-                buffer: Blazor.platform.readInt32Field(readFileParamsPointer, 32)
+                position: Blazor.platform.readUint64Field(readFileParamsPointer, 24)
+            };
+        };
+        this.ReadBufferPointer = function (readBufferPointer) {
+            return {
+                taskId: Blazor.platform.readUint64Field(readBufferPointer, 0),
+                buffer: Blazor.platform.readInt32Field(readBufferPointer, 8)
             };
         };
         this.ReadFileUnmarshalledAsync = function (readFileParamsPointer) {
@@ -101,22 +123,27 @@ var FileReaderComponent = (function () {
             var asyncCall = new Promise(function (resolve, reject) {
                 return _this.ReadFileSlice(readFileParams, function (r, b) { return r.readAsArrayBuffer(b); })
                     .then(function (r) {
-                    try {
-                        var dotNetBufferView = Blazor.platform.toUint8Array(readFileParams.buffer);
-                        var arrayBuffer = r.result;
-                        dotNetBufferView.set(new Uint8Array(arrayBuffer), readFileParams.bufferOffset);
-                        var byteCount = Math.min(arrayBuffer.byteLength, readFileParams.count);
-                        resolve(byteCount);
-                    }
-                    catch (e) {
-                        reject(e);
-                    }
+                    _this.readResultByTaskId.set(readFileParams.taskId, {
+                        arrayBuffer: r.result,
+                        params: readFileParams
+                    });
+                    resolve();
                 }, function (e) { return reject(e); });
             });
-            asyncCall.then(function (byteCount) { return DotNet.invokeMethodAsync("Tewr.Blazor.FileReader", "EndReadFileUnmarshalledAsyncResult", readFileParams.taskId, byteCount); }, function (error) {
+            asyncCall.then(function () { return FileReaderJsInterop.endTask(readFileParams.taskId); }, function (error) {
                 console.error("ReadFileUnmarshalledAsync error", error);
-                DotNet.invokeMethodAsync("Tewr.Blazor.FileReader", "EndReadFileUnmarshalledAsyncError", readFileParams.taskId, error.toString());
+                DotNet.invokeMethodAsync(FileReaderJsInterop.assembly, "EndReadFileUnmarshalledAsyncError", readFileParams.taskId, error.toString());
             });
+            return 0;
+        };
+        this.FillBufferUnmarshalled = function (bufferPointer) {
+            var readBufferParams = _this.ReadBufferPointer(bufferPointer);
+            var dotNetBufferView = Blazor.platform.toUint8Array(readBufferParams.buffer);
+            var data = _this.readResultByTaskId.get(readBufferParams.taskId);
+            _this.readResultByTaskId.delete(readBufferParams.taskId);
+            dotNetBufferView.set(new Uint8Array(data.arrayBuffer), data.params.bufferOffset);
+            var byteCount = Math.min(data.arrayBuffer.byteLength, data.params.count);
+            return byteCount;
         };
         this.ReadFileMarshalledAsync = function (readFileParams) {
             return new Promise(function (resolve, reject) {
@@ -156,7 +183,7 @@ var FileReaderComponent = (function () {
     }
     FileReaderComponent.prototype.LogIfNull = function (element) {
         if (element == null) {
-            console.log("Tewr.Blazor.FileReader: HTMLElement is null. Can't access IFileReaderRef after HTMLElement was removed from DOM.");
+            console.log(FileReaderJsInterop.assembly + ": HTMLElement is null. Can't access IFileReaderRef after HTMLElement was removed from DOM.");
         }
     };
     FileReaderComponent.prototype.GetFiles = function (element) {
@@ -182,7 +209,7 @@ var FileReaderComponent = (function () {
         };
         var properties = {};
         for (var property in file) {
-            if (Object.getPrototypeOf(file).hasOwnProperty(property) && !(property in result)) {
+            if (Object.prototype.hasOwnProperty.call(file, property) && !(property in result)) {
                 properties[property] = file[property];
             }
         }
