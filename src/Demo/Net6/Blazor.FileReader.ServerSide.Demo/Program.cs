0d7785f7860402f0e7183f3cac1080a20cd70e3b
Tor KNUTSSON <Tewr@users.noreply.github.com>
2021-12-14 22:31:40 +0100

    Server-side project

---
+++ src/Demo/Net6/Blazor.FileReader.ServerSide.Demo/Program.cs
@@ -0,0 +1,33 @@
+using Blazor.FileReader.ServerSide.Demo.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Tewr.Blazor.FileReader;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddFileReaderService();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
