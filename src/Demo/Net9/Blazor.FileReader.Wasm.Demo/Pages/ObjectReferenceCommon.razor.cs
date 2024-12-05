3c7867c7a071cff20aac7ef9304909ab26051473
Tewr <Tewr@users.noreply.github.com>
2024-12-05 22:58:55 +0100

    Adds .net9 support. Closes #200.

---
+++ src/Demo/Net9/Blazor.FileReader.Wasm.Demo/Pages/ObjectReferenceCommon.razor.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Tewr.Blazor.FileReader;
+
+namespace Blazor.FileReader.Demo.Common
+{
+    public partial class ObjectReferenceCommonBase : ComponentBase
+    {
+        [Inject]
+        IFileReaderService fileReaderService { get; set; }
+        [Inject]
+        IJSRuntime CurrentJSRuntime { get; set; }
+
+        public ElementReference inputElement;
+        public string Output { get; set; }
+
+        public async Task ReadFile()
+        {
+            Output = string.Empty;
+            this.StateHasChanged();
+            var files = await fileReaderService.CreateReference(inputElement).EnumerateFilesAsync();
+            var isCustomGetLengthMethodInitialized =  await CurrentJSRuntime.InvokeAsync<bool>("window.hasOwnProperty", "customGetLengthMethod");
+            if (!isCustomGetLengthMethodInitialized)
+            {
+                await WriteLine("Defining custom method 'customGetLengthMethod'");
+                await CurrentJSRuntime.InvokeVoidAsync("eval", 
+                    @"window.customGetLengthMethod = (jsFileFromDotNet) => {
+                            return jsFileFromDotNet.size;
+                    };");
+            }
+
+            if (!files.Any())
+            {
+                await WriteLine("No file selected");
+            }
+
+            foreach (var file in files)
+            {
+                var jsFile = await file.GetJSObjectReferenceAsync();
+                await using (jsFile)
+                {
+                    
+                    // get length manually using a custom method
+                    var length = await this.CurrentJSRuntime.InvokeAsync<long>("customGetLengthMethod", jsFile);
+                    await WriteLine($"Length from customGetLengthMethod: {length}");
+                }
+
+            }
+        }
+
+        public async Task WriteLine(string log)
+        {
+            Output += $"{log}{Environment.NewLine}";
+            await InvokeAsync(StateHasChanged);
+        }
+    }
+}
---
+++ src/Demo/Net9/Blazor.FileReader.Wasm.Demo/Program.cs
@@ -0,0 +1,13 @@
+using Blazor.FileReader.Wasm.Demo;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Tewr.Blazor.FileReader;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddFileReaderService();
+
+await builder.Build().RunAsync();
